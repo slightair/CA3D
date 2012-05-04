ec4ed34d4859f77054fff7d722354ab733fe1b34
Tomohiro Moro <arksutite@gmail.com>
2012-05-04 15:51:56 +0900

    setup OpenGLView

--- CA3D/CA3DWorldView.m
+++ CA3D/CA3DWorldView.m
@@ -7,25 +7,78 @@
 //
 
 #import "CA3DWorldView.h"
+#import <QuartzCore/QuartzCore.h>
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
 
 @implementation CA3DWorldView
+{
+    CAEAGLLayer *eaglLayer_;
+    EAGLContext *context_;
+    GLuint colorRenderBuffer_;
+}
+
++ (Class)layerClass
+{
+    return [CAEAGLLayer class];
+}
+
+- (void)setupLayer
+{
+    eaglLayer_ = (CAEAGLLayer *)self.layer;
+    eaglLayer_.opaque = YES;
+}
+
+- (void)setupContext
+{
+    EAGLRenderingAPI api = kEAGLRenderingAPIOpenGLES2;
+    context_ = [[EAGLContext alloc] initWithAPI:api];
+    
+    if (!context_) {
+        NSLog(@"Failed to initialize OpenGLES 2.0 context");
+        exit(1);
+    }
+    
+    if (![EAGLContext setCurrentContext:context_]) {
+        NSLog(@"Failed to set current OpenGL context");
+        exit(1);
+    }
+}
+
+- (void)setupRenderBuffer
+{
+    glGenRenderbuffers(1, &colorRenderBuffer_);
+    glBindRenderbuffer(GL_RENDERBUFFER, colorRenderBuffer_);
+    [context_ renderbufferStorage:GL_RENDERBUFFER fromDrawable:eaglLayer_];
+}
+
+- (void)setupFrameBuffer
+{
+    GLuint frameBuffer;
+    glGenFramebuffers(1, &frameBuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, colorRenderBuffer_);
+}
+
+- (void)render
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    [context_ presentRenderbuffer:GL_RENDERBUFFER];
+}
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        [self setupLayer];
+        [self setupContext];
+        [self setupRenderBuffer];
+        [self setupFrameBuffer];
+        [self render];
     }
     return self;
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
 
 @end
