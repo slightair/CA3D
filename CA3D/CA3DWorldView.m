f71c72e5399db8e1d15fd60fe3bee872bd92f2be
Tomohiro Moro <arksutite@gmail.com>
2012-05-05 23:09:23 +0900

    なんかでた

--- CA3D/CA3DWorldView.m
+++ CA3D/CA3DWorldView.m
@@ -11,11 +11,30 @@
 #import <OpenGLES/ES2/gl.h>
 #import <OpenGLES/ES2/glext.h>
 
+typedef struct {
+    float Position[3];
+    float Color[4];
+} Vertex;
+
+const Vertex Vertices[] = {
+    {{ 1, -1, 0}, {1, 0, 0, 1}},
+    {{ 1,  1, 0}, {0, 1, 0, 1}},
+    {{-1,  1, 0}, {0, 0, 1, 1}},
+    {{-1, -1, 0}, {0, 0, 0, 1}}
+};
+
+const GLubyte Indices[] = {
+    0, 1, 2,
+    2, 3, 0
+};
+
 @implementation CA3DWorldView
 {
     CAEAGLLayer *eaglLayer_;
     EAGLContext *context_;
     GLuint colorRenderBuffer_;
+    GLuint positionSlot_;
+    GLuint colorSlot_;
 }
 
 + (Class)layerClass
@@ -64,7 +83,89 @@
 {
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    glViewport(0, 0, self.frame.size.width, self.frame.size.height);
+    
+    glVertexAttribPointer(positionSlot_, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL);
+    glVertexAttribPointer(colorSlot_, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid *)(sizeof(float) * 3));
+    
+    glDrawElements(GL_TRIANGLES, sizeof(Indices) / sizeof(Indices[0]), GL_UNSIGNED_BYTE, NULL);
+    
     [context_ presentRenderbuffer:GL_RENDERBUFFER];
+}
+
+- (GLuint)compileShader:(NSString *)shaderName withType:(GLenum)shaderType
+{
+    NSString *shaderPath = [[NSBundle mainBundle] pathForResource:shaderName ofType:@"glsl"];
+    NSError *error = nil;
+    NSString *shaderString = [NSString stringWithContentsOfFile:shaderPath encoding:NSUTF8StringEncoding error:&error];
+    
+    if (!shaderString) {
+        NSLog(@"Error loading shader: %@", error.localizedDescription);
+        exit(1);
+    }
+    
+    GLuint shaderHandle = glCreateShader(shaderType);
+    
+    const char *shaderStringUTF8 = [shaderString UTF8String];
+    int shaderStringLength = [shaderString length];
+    
+    glShaderSource(shaderHandle, 1, &shaderStringUTF8, &shaderStringLength);
+    
+    glCompileShader(shaderHandle);
+    
+    GLint compileSuccess = GL_FALSE;
+    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &compileSuccess);
+    if (compileSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetShaderInfoLog(shaderHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    return shaderHandle;
+}
+
+- (void)compileShaders
+{
+    GLuint vertexShader = [self compileShader:@"SimpleVertex" withType:GL_VERTEX_SHADER];
+    GLuint fragmentShader = [self compileShader:@"SimpleFragment" withType:GL_FRAGMENT_SHADER];
+    
+    GLuint programHandle = glCreateProgram();
+    glAttachShader(programHandle, vertexShader);
+    glAttachShader(programHandle, fragmentShader);
+    glLinkProgram(programHandle);
+    
+    GLint linkSuccess = GL_FALSE;
+    glGetProgramiv(programHandle, GL_LINK_STATUS, &linkSuccess);
+    if (linkSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetProgramInfoLog(programHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    glUseProgram(programHandle);
+    
+    positionSlot_ = glGetAttribLocation(programHandle, "Position");
+    colorSlot_ = glGetAttribLocation(programHandle, "SourceColor");
+    glEnableVertexAttribArray(positionSlot_);
+    glEnableVertexAttribArray(colorSlot_);
+}
+
+- (void)setupVertexBufferObjects
+{
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    
+    GLuint indexBuffer;
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
 }
 
 - (id)initWithFrame:(CGRect)frame
@@ -76,6 +177,10 @@
         [self setupContext];
         [self setupRenderBuffer];
         [self setupFrameBuffer];
+        
+        [self compileShaders];
+        [self setupVertexBufferObjects];
+        
         [self render];
     }
     return self;
